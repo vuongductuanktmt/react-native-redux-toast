87af429b9e0318ed722e5b5fe0438444b5f513f9
vuongductuanktmt <anhtuanpro312@gmail.com>
2019-05-29 17:58:29 +0700

    Update reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -6,6 +6,7 @@ const defaultState = {
   message: null,
   error: false,
   warning: false,
+  success: false,
   duration: null
 };
 
@@ -14,13 +15,15 @@ export default function reducer(state = Immutable(defaultState), action) {
     case actions.HIDE:
     case actions.DISPLAY_ERROR:
     case actions.DISPLAY_WARNING:
+    case actions.DISPLAY_SUCCESS:
     case actions.DISPLAY_INFO: {
       return state.merge(
         {
           message: action.payload.message,
           duration: action.payload.duration,
           error: action.type === actions.DISPLAY_ERROR,
-          warning: action.type === actions.DISPLAY_WARNING
+          warning: action.type === actions.DISPLAY_WARNING,
+          success: action.type === actions.DISPLAY_SUCCESS
         },
         { deep: true }
       );
